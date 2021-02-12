69d7ef82ca7a132d293e217d635b192e96880131
nblack0917 <nickablack@gmail.com>
2021-02-12 12:04:09 -0600

    reset to base code

--- script.js
+++ script.js
@@ -2,14 +2,24 @@ function myFunction() {
     var y = document.getElementById("lineContainer")
     var x = document.getElementById("myTopNav");
     var z = document.getElementById("navbar")
+    var a = document.getElementById("midNav")
 
     y.classList.toggle("change");
 
     if (x.className === "nav-block") {
         x.className += " responsive";
         z.className += " responsive";
+        a.className += " loading";
     } else {
         x.className = "nav-block";
         z.className = "nav";
+        a.className = "nav-links";
     }
+}
+
+function copyToClipboard(element) {
+    console.log("copied!");
+    let clipText = element.parentNode.innerText;
+    console.log(clipText)
+    navigator.clipboard.writeText(clipText)
 }
